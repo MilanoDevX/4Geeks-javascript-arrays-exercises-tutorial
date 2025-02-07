9d17a779b0fb9da1d4cac32a4231a0ffcbedb21a
MilanoTester <eliasmilano.tester@gmail.com>
2025-02-06 21:38:41 -0300

    exercises 3 to 4.3

--- .learn/resets/04.3-Add_items_to_array/app.js
+++ .learn/resets/04.3-Add_items_to_array/app.js
@@ -1,9 +1,5 @@
 let arr = [4,5,734,43,45];
 
 // Your code here, use the push() function and the 2 Math functions
-for (let i=1; i<=2; i++) {
-let number = Math.floor(Math.random()*10);
-arr.push(number);
-}
 
 console.log(arr);
